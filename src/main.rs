63c57beae20d16f44aa77360990c438ec7d71eaa
Toni Alatalo <toni@playsign.net>
2023-10-06 17:08:12 +0300

    sprite_movement

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,47 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, sprite_movement)
         .run();
+}
+
+#[derive(Component)]
+enum Direction {
+    Up,
+    Down,
 }
 
 fn setup(
     mut commands: Commands,
 ) {
     commands.spawn(Camera2dBundle::default());
-
-    // Rectangle
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(50.0, 100.0)),
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(50.0, 100.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
             ..default()
         },
-        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
-        ..default()
-    });
+        Direction::Up
+    ));
+}
+
+/// The sprite is animated by changing its translation depending on the time that has passed since
+/// the last frame.
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut sprite, mut transform) in &mut sprite_position {
+        match *sprite {
+            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
+            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
+        }
+
+        if transform.translation.y > 200. {
+            *sprite = Direction::Down;
+        } else if transform.translation.y < -200. {
+            *sprite = Direction::Up;
+        }
+    }
 }
