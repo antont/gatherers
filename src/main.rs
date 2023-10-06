4f072148e787b2aa64d5adbf3a7fad3a704025a2
Toni Alatalo <toni@playsign.net>
2023-10-06 17:01:06 +0300

    bevy hello world: draw a rectangle sprite

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use bevy::{prelude::*};
+
+fn main() {
+    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Rectangle
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 100.0)),
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+        ..default()
+    });
+}
