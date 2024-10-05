6810b2b8d3aadc19474ba71a88be0e4d344b9548
Toni Alatalo <toni.alatalo@gmail.com>
2024-10-05 14:22:14 +0300

    SpatialIndex wip

---
+++ src/spatial_index.rs
@@ -0,0 +1,37 @@
+use bevy::prelude::*;
+use std::collections::{HashMap, HashSet};
+
+#[derive(Resource, Default)]
+pub struct SpatialIndex {
+    map: HashMap<(i32, i32), HashSet<Entity>>,
+}
+
+/// Cell size has to be bigger than any `TriggerMine::radius`
+const CELL_SIZE: f32 = 64.0;
+
+impl SpatialIndex {
+    // Lookup all entities within adjacent cells of our spatial index
+    pub fn get_nearby(&self, pos: Vec2) -> Vec<Entity> {
+        let tile = (
+            (pos.x / CELL_SIZE).floor() as i32,
+            (pos.y / CELL_SIZE).floor() as i32,
+        );
+        let mut nearby = Vec::new();
+        for x in -1..2 {
+            for y in -1..2 {
+                if let Some(mines) = self.map.get(&(tile.0 + x, tile.1 + y)) {
+                    nearby.extend(mines.iter());
+                }
+            }
+        }
+        nearby
+    }
+
+    pub fn update_spatial_index(&mut self, entity: Entity, pos: Vec2) {
+        let tile = (
+            (pos.x / CELL_SIZE).floor() as i32,
+            (pos.y / CELL_SIZE).floor() as i32,
+        );
+        self.map.entry(tile).or_default().insert(entity);
+    }
+}
